6506617d4f03d0b67993a83e91cf62b551df6e98
SammyKrosoft <sammy@hotmail.fr>
2019-12-20 18:05:58 -0500

    create function

--- EncodeToBase64.ps1
+++ EncodeToBase64.ps1
@@ -16,3 +16,34 @@ Exit
 
 # To decode the Base64 string to its original file type, just convert from Base64, writing all bytes:
 [IO.File]::WriteAllBytes($DestFileName, [Convert]::FromBase64String($base64string))
+
+
+# Desinging functions from the above for re-useability
+
+Function EncodeBase64ToFile {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)][String] $FilePath ,
+        [Parameter(Mandatory = $false)][String]$DestinationBase64StringFile = "$($Env:USERPROFILE)\Documents\Base64EncodedStringFile.txt"
+    )
+    
+    $Base64String = [Convert]::ToBase64String([IO.File]::ReadAllBytes($FilePath))
+
+    Set-Content -Value $Base64String -Path $DestinationBase64StringFile
+
+}
+
+Function DecodeBase64StringFromFile {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [String]
+        $FilePathContainingBase64Code,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [String]
+        $DestinationFile
+    )
+
+    [IO.File]::WriteAllBytes($DestinationFile, [Convert]::FromBase64String(Get-Content -Path $FilePathContainingBase64Code))
+    
+}
