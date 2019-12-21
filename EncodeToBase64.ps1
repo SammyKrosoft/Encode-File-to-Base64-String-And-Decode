bed7a393bad5fbb94ff7432029bc9c22d8fb31f6
SammyKrosoft <sammy@hotmail.fr>
2019-12-20 20:57:29 -0500

    functions enhancements

--- EncodeToBase64.ps1
+++ EncodeToBase64.ps1
@@ -28,19 +28,22 @@ Function EncodeBase64ToFile {
         [Parameter(Mandatory = $true, Position = 0)][String] $FilePath ,
         [Parameter(Mandatory = $false)][String]$DestinationBase64StringFile = "$($Env:USERPROFILE)\Documents\Base64EncodedStringFile.txt"
     )
-    
-    Write-Debug "Beginning Try sequence with current options $FilePath and $DestinationBase64StringFile ..."
+    #SamHeader
+    $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Write-Verbose "Beginning Try sequence with current options $FilePath and $DestinationBase64StringFile ..."
     Try {
-        Write-Debug "Trying to convert file specified to Base64 string... it can be long if the file you try to encode is big !"
+        Write-Verbose "Trying to convert file specified to Base64 string... it can be long if the file you try to encode is big !"
         $Base64String = [Convert]::ToBase64String([IO.File]::ReadAllBytes($FilePath))
-        Write-Debug "Finished converting with success !"
+        Write-Verbose "Finished converting with success !"
+        Write-Verbose "Pouring the Base64 generated code into the destination file $DestinationBase64StringFile"
+        Set-Content -Value $Base64String -Path $DestinationBase64StringFile
+        Write-Host "Done !" -ForegroundColor Green
     } Catch {
-        Write-Debug "Something went wrong ... We're in the CATCH section ..."
-        Write-Host "File specified $FilePath not found ... please use an existing file path and try again !" -ForegroundColor Red
+        Write-Verbose "Something went wrong ... We're in the CATCH section ..."
+        Write-Host "File specified $FilePath not found ... please use an existing file path and try again !" -ForegroundColor Yellow -BackgroundColor Red
     }
-
-    Write-Debug "Pouring the Base64 generated code into the destination file $DestinationBase64StringFile"
-    Set-Content -Value $Base64String -Path $DestinationBase64StringFile
+    $StopWatch.Stop()
+    $StopWatch.Elapsed | Fl TotalSeconds
 
 }
 
@@ -55,9 +58,18 @@ Function DecodeBase64StringFromFile {
         $DestinationFile
     )
 
-    $Base64Content = Get-Content -Path $FilePathContainingBase64Code
-    [IO.File]::WriteAllBytes($DestinationFile, [Convert]::FromBase64String($Base64Content))
+    $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Write-Verbose "Beginning TRY sequence with parameters specified -FilePathContainingBase64Code as $FilePathContainingBase64Code and -DestinationFile as $DestinationFile"
 
+    Try {
+        $Base64Content = Get-Content -Path $FilePathContainingBase64Code
+        [IO.File]::WriteAllBytes($DestinationFile, [Convert]::FromBase64String($Base64Content))
+    } Catch {
+        Write-Verbose "Something went wrong ... We're in the CATCH section..."
+        Write-Host "File specified $FilePathContainingBase64Code inexistant or destination file $Destination not specified ..." -ForegroundColor Yellow -BackgroundColor Red
+    }
+    $StopWatch.Stop()
+    $StopWatch.Elapsed | fl TotalSeconds
 }
 
 # Example with the above strings (file Timber.exe)
@@ -68,6 +80,5 @@ $FileName = "C:\Users\sammy\OneDrive\Utils\timber.exe"
 # The full path of the file we will decode the Base64 to "rebuild" that file
 $DestBase64EncodedFile= "C:\Users\sammy\OneDrive\Utils\timberConv.txt"
 
-EncodeBase64ToFile -FilePath $FileName -DestinationBase64StringFile $DestBase64EncodedFile -Verbose
-
-# DecodeBase64StringFromFile -FilePathContainingBase64Code $DestBase64EncodedFile -DestinationFile $FileName
+ #EncodeBase64ToFile -FilePath $FileName -DestinationBase64StringFile $DestBase64EncodedFile -Verbose
+DecodeBase64StringFromFile -FilePathContainingBase64Code $DestBase64EncodedFile -DestinationFile $FileName
